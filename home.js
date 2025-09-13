3fd3425cab1c13462641bf5a5f082b5714ab9b3d
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-09-13 13:08:03 -0300

    sabado

--- home.js
+++ home.js
@@ -1,12 +1,14 @@
 let botaoSeta = document.getElementById('selectSeta');
-let classeAntiga = document.querySelector('.box-select-cidades');
-let classeNova = document.querySelector('.box-select-cidadesNova');
 let clique = true;
-selectSeta.addEventListener('click', () => {
+botaoSeta.addEventListener('click', () => {
+    let classeAntiga = document.querySelector('.box-select-cidades');
+    let classeNova = document.querySelector('.box-select-cidadesNova');
     if(clique){
         classeAntiga.classList.replace('box-select-cidades', 'box-select-cidadesNova');
-        clique = true;
-    } else {
         clique = false;
+    } else {
+        classeNova.classList.replace('box-select-cidadesNova', 'box-select-cidades');
+        clique = true;
+        console.log('estou aqui')
     }
 });
