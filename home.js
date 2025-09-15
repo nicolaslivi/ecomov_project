bfe4ca68f6129252b4feb3e7ef7838638dd75e42
Nicolas Liviera <nicolasrlivi@gmail.com>
2025-09-15 20:26:15 -0300

    aula 15/09 - parte 2

--- home.js
+++ home.js
@@ -4,17 +4,21 @@ function novaBoxCidades (){
     let boxNova = document.querySelector('.box-select-cidadesNova');
     let cidadesAntiga = document.querySelectorAll('.botaoEscolhaCidade');
     let cidadesNova = document.querySelectorAll('.botaoEscolhaCidadeNova');
+    let scrowAntigo = document.querySelector('.scrowBoxCidades');
+    let scrowNovo = document.querySelector('.scrowBoxCidadesNova')
     if(clique){
         boxAntiga.classList.replace('box-select-cidades', 'box-select-cidadesNova');
         cidadesAntiga.forEach(cidade => {
             cidade.classList.replace('botaoEscolhaCidade', 'botaoEscolhaCidadeNova')
         });
+        scrowAntigo.classList.replace('scrowBoxCidades', 'scrowBoxCidadesNova');
         clique = false;
     } else {
         boxNova.classList.replace('box-select-cidadesNova', 'box-select-cidades');
         cidadesNova.forEach(cidade  => {
             cidade.classList.replace('botaoEscolhaCidadeNova', 'botaoEscolhaCidade')
         });
+        scrowNovo.classList.replace('scrowBoxCidadesNova', 'scrowBoxCidades');
         clique = true;
     }
 };
